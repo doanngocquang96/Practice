3ae9da80de2980d16c845bd041b07a01c5811748
Quang Doan <doanngocquang96@gmail.com>
2022-10-21 20:54:43 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(String.valueOf(123));
+		System.out.println(String.valueOf(123));
+		System.out.println(String.valueOf(123));
+		System.out.println(String.valueOf(123));
+		
+	}
+}
