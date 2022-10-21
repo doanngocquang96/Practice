0416151c9760cac463d845bd984fc2009374157b
Quang Doan <doanngocquang96@gmail.com>
2022-10-21 21:15:16 +0700

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println(String.valueOf(123));
-		System.out.println(String.valueOf(123));
-		System.out.println(String.valueOf(123));
-		System.out.println(String.valueOf(123));
+		System.out.println(String.valueOf(123456));
 		
+		
+		
+		
+		//them 1
+		//them 2
 	}
 }
